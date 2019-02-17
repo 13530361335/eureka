3dde53df40ae26bf2c3b3c46b74a8f979118e779
Joker Jing <761878367@qq.com>
2019-02-17 19:53:21 +0800

    增加集群

--- src/main/java/com/springcloud/eureka/EurekaApplication.java
+++ src/main/java/com/springcloud/eureka/EurekaApplication.java
@@ -3,13 +3,23 @@ package com.springcloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+public class EurekaApplication extends WebSecurityConfigurerAdapter{
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
     }
 
 }
